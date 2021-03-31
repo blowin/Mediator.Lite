b879aee15e55b4bbee20942386d1d874b6339080
blowin <dsessence@gmail.com>
2021-03-31 16:40:33 +0300

    remove IFactory

--- Mediator.Lite/Extension/MediatorBuilderNotificationHandlerExt.cs
+++ Mediator.Lite/Extension/MediatorBuilderNotificationHandlerExt.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Mediator.Lite.Abstraction;
-using Mediator.Lite.Implementation.Factory;
-using Mediator.Lite.Implementation.NotificationHandler;
+﻿using Mediator.Lite.Abstraction;
 
 namespace Mediator.Lite.Extension
 {
@@ -10,22 +7,5 @@ namespace Mediator.Lite.Extension
         public static DictionaryServiceFactoryBuilder AddNotificationHandler<TNotification>(this DictionaryServiceFactoryBuilder self, INotificationHandler<TNotification> handler) 
             where TNotification : INotification =>
             self.AddNotificationHandler(typeof(TNotification), handler);
-
-        public static DictionaryServiceFactoryBuilder AddNotificationHandlerAsLazy<TNotification>(this DictionaryServiceFactoryBuilder self,
-            Func<INotificationHandler<TNotification>> handlerFactory, bool isThreadSafe = true)
-            where TNotification : INotification
-        {
-            return self.AddNotificationHandler(
-                typeof(TNotification), 
-                NotificationHandlerWithFactory.Create<LazyFactory<INotificationHandler<TNotification>>, TNotification>(
-                    new LazyFactory<INotificationHandler<TNotification>>(
-                        new Lazy<INotificationHandler<TNotification>>(
-                            handlerFactory, 
-                            isThreadSafe
-                        )
-                    )
-                )
-            );
-        }
     }
 }
--- Mediator.Lite/Extension/MediatorBuilderRequestHandlerExt.cs
+++ Mediator.Lite/Extension/MediatorBuilderRequestHandlerExt.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Mediator.Lite.Abstraction;
-using Mediator.Lite.Implementation.Factory;
-using Mediator.Lite.Implementation.RequestHandler;
+﻿using Mediator.Lite.Abstraction;
 
 namespace Mediator.Lite.Extension
 {
@@ -10,22 +7,5 @@ namespace Mediator.Lite.Extension
         public static DictionaryServiceFactoryBuilder AddRequestHandler<TRequest, TResponse>(this DictionaryServiceFactoryBuilder self, IRequestHandler<TRequest, TResponse> handler) 
             where TRequest : IRequest<TResponse> =>
             self.AddRequestHandler(typeof(TRequest), handler);
-
-        public static DictionaryServiceFactoryBuilder AddRequestHandlerAsLazy<TRequest, TResponse>(this DictionaryServiceFactoryBuilder self,
-            Func<IRequestHandler<TRequest, TResponse>> handlerFactory, bool isThreadSafe = true)
-            where TRequest : IRequest<TResponse>
-        {
-            return self.AddRequestHandler(
-                typeof(TRequest), 
-                RequestHandlerWithFactory.Create<LazyFactory<IRequestHandler<TRequest, TResponse>>, TRequest, TResponse>(
-                        new LazyFactory<IRequestHandler<TRequest, TResponse>>(
-                                new Lazy<IRequestHandler<TRequest, TResponse>>(
-                                        handlerFactory, 
-                                        isThreadSafe
-                                    )
-                            )
-                    )
-                );
-        }
     }
 }
